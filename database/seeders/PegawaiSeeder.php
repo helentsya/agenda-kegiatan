f01cc82ea0857a0aed56bb744d4ce029343d20bf
akhmadazhar <akhmadazharnizami@gmail.com>
2024-07-19 15:14:51 +0800

    seed

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -15,89 +15,41 @@ class PegawaiSeeder extends Seeder
     {
         DB::table('pegawais')->insert([
             [
-                'id' => 2,
-                'id_jabatan' => 4,
-                'id_bidang' => 2,
-                'nip' => '987654321',
-                'nama_pegawai' => 'Ani',
-                'jenis_kelamin' => 'Perempuan',
-                'tempat_lahir' => 'Bandung',
+                'id' => 1,
+                'id_jabatan' => 0,
+                'id_bidang' => 0,
+                'nip' => '0',
+                'nama_pegawai' => 'Admin',
+                'jenis_kelamin' => 'Laki - Laki',
+                'tempat_lahir' => 'Banjarmasin',
                 'tanggal_lahir' => '1991-01-01',
-                'jabatan' => 'Staff',
+                'jabatan' => 'Admin',
+                'alamat' => 'Jl. Jendral Sudirman'
+            ],
+            [
+                'id' => 2,
+                'id_jabatan' => 1,
+                'id_bidang' => 1,
+                'nip' => '00000',
+                'nama_pegawai' => 'Kepala Dinas',
+                'jenis_kelamin' => 'Laki - Laki',
+                'tempat_lahir' => 'Banjarmasin',
+                'tanggal_lahir' => '1996-01-01',
+                'jabatan' => 'Kepala Dinas',
                 'alamat' => 'Jl. Jendral Sudirman'
             ],
             [
                 'id' => 3,
-                'id_jabatan' => 2,
-                'id_bidang' => 3,
-                'nip' => '123123123',
-                'nama_pegawai' => 'Cici',
-                'jenis_kelamin' => 'Perempuan',
-                'tempat_lahir' => 'Surabaya',
-                'tanggal_lahir' => '1992-01-01',
-                'jabatan' => 'Staff',
-                'alamat' => 'Jl. Jendral Sudirman'
-            ],
-            [
-                'id' => 4,
-                'id_jabatan' => 3,
-                'id_bidang' => 5,
-                'nip' => '123321',
-                'nama_pegawai' => 'Andri May',
-                'jenis_kelamin' => 'Laki-laki',
-                'tempat_lahir' => 'Marabahan',
-                'tanggal_lahir' => '2024-06-14',
-                'jabatan' => 'Pegawai',
-                'alamat' => 'Marabahan'
-            ],
-            [
-                'id' => 5,
-                'id_jabatan' => 4,
-                'id_bidang' => 2,
-                'nip' => '123',
-                'nama_pegawai' => 'Alghi Nuub',
-                'jenis_kelamin' => 'Laki-laki',
-                'tempat_lahir' => 'Bbjb',
-                'tanggal_lahir' => '2003-02-22',
-                'jabatan' => 'HRD',
-                'alamat' => 'Bbjb'
-            ],
-            [
-                'id' => 6,
-                'id_jabatan' => 7,
-                'id_bidang' => 4,
-                'nip' => '3021',
-                'nama_pegawai' => 'Pegawai Sekretaris',
-                'jenis_kelamin' => 'Laki-laki',
-                'tempat_lahir' => 'Rantau',
-                'tanggal_lahir' => '2005-01-13',
-                'jabatan' => 'Pegawai Sekretaris',
-                'alamat' => 'Bjb'
-            ],
-            [
-                'id' => 7,
-                'id_jabatan' => 3,
-                'id_bidang' => 1,
-                'nip' => '1',
-                'nama_pegawai' => 'Muhammad Sumitra',
-                'jenis_kelamin' => 'Laki-laki',
-                'tempat_lahir' => 'Banjarbaru',
-                'tanggal_lahir' => '2013-01-09',
-                'jabatan' => 'Kepala Bidang',
-                'alamat' => 'Banjarbaru'
-            ],
-            [
-                'id' => 8,
                 'id_jabatan' => 3,
                 'id_bidang' => 3,
-                'nip' => '6969',
-                'nama_pegawai' => 'Ismail Ahmad Kanam',
-                'jenis_kelamin' => 'Laki-laki',
-                'tempat_lahir' => 'Banjarbaru',
-                'tanggal_lahir' => '2002-05-29',
+                'nip' => '0003',
+                'nama_pegawai' => 'Muhammad Ikhlas',
+                'jenis_kelamin' => 'Laki - Laki',
+                'tempat_lahir' => 'Banjarmasin',
+                'tanggal_lahir' => '1995-01-01',
                 'jabatan' => 'Kepala Bidang Informatika',
-                'alamat' => 'Banjarbaru'
-            ]
+                'alamat' => 'Gambut'
+            ],
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,82 +16,38 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([
             [
+                'id' => 1,
+                'id_jabatan' => 0,
+                'id_pegawai' => 1,
+                'id_bidang' => 0,
+                'nama_user' => 'Admin',
+                'username' => 'admin',
+                'password' => Hash::make('TI_Poliban'),
+                'email' => 'admin@example.com',
+                'roles' => 'admin'
+            ],
+            [
                 'id' => 2,
-                'id_jabatan' => 2,
+                'id_jabatan' => 1,
                 'id_pegawai' => 2,
                 'id_bidang' => 1,
-                'nama_user' => 'Ani',
-                'username' => 'pegawai1',
-                'password' => Hash::make('pegawai1'),
-                'email' => 'pegawai1@test.com',
-                'roles' => 'pegawai'
+                'nama_user' => 'Kepala Dinas',
+                'username' => 'kepaladinas',
+                'password' => Hash::make('KepalaDinas'),
+                'email' => 'kepaladinas@gmail.com',
+                'roles' => 'kepalapejabat'
             ],
             [
                 'id' => 3,
                 'id_jabatan' => 3,
                 'id_pegawai' => 3,
-                'id_bidang' => 0,
-                'nama_user' => 'Cici',
-                'username' => 'kepalapejabat',
-                'password' => Hash::make('kepalapejabat'),
-                'email' => 'pegawai2@test.com',
-                'roles' => 'kepalapejabat'
-            ],
-            [
-                'id' => 4,
-                'id_jabatan' => 7,
-                'id_pegawai' => 4,
-                'id_bidang' => 5,
-                'nama_user' => 'Andri May',
-                'username' => 'andri',
-                'password' => Hash::make('andri'),
-                'email' => 'andri@gmail.com',
-                'roles' => 'bidang'
-            ],
-            [
-                'id' => 5,
-                'id_jabatan' => 4,
-                'id_pegawai' => 5,
-                'id_bidang' => 1,
-                'nama_user' => 'Alghi Nuub',
-                'username' => 'ColonialGT',
-                'password' => Hash::make('alghi'),
-                'email' => 'alghi@gmail.com',
-                'roles' => 'bidang'
-            ],
-            [
-                'id' => 6,
-                'id_jabatan' => 7,
-                'id_pegawai' => 6,
-                'id_bidang' => 1,
-                'nama_user' => 'Pegawai Sekretaris',
-                'username' => 'pegawaisekre',
-                'password' => Hash::make('pegawaisekre'),
-                'email' => 'pegawaisekre@gmail.co',
-                'roles' => 'bidang'
-            ],
-            [
-                'id' => 7,
-                'id_jabatan' => 3,
-                'id_pegawai' => 7,
-                'id_bidang' => 1,
-                'nama_user' => 'Muhammad Sumitra',
-                'username' => 'kepalasekre',
-                'password' => Hash::make('kepalasekre'),
-                'email' => 'kepalasekre@gmail.com',
-                'roles' => 'pegawai'
-            ],
-            [
-                'id' => 8,
-                'id_jabatan' => 2,
-                'id_pegawai' => 9,
                 'id_bidang' => 3,
-                'nama_user' => 'Ismail Ahmad Kanam',
-                'username' => 'kepalakomunikasi',
-                'password' => Hash::make('kepalakomunikasi'),
-                'email' => 'kepalakomunikasi@gmail.com',
+                'nama_user' => 'Muhammad Ikhlas',
+                'username' => 'kabidinfo',
+                'password' => Hash::make('kabidinfo@123'),
+                'email' => 'kabidinfo123@gmail.com',
                 'roles' => 'pegawai'
-            ]
+            ],
         ]);
     }
 }
